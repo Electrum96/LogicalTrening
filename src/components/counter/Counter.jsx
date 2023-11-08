e3e6f2dd5ead7f69b844bde17c1cd2c2cc21c528
electrum <kachalina.kate@mail.ru>
2023-11-08 15:30:36 +0300

    08-11-2023:15:30

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,6 +1,6 @@
 import {Text, View, Image, TouchableOpacity} from 'react-native';
 
-import mobxCounterSlice from '../../../mobx/mobxCounterSlice';
+import mobxCounterSlice from '../../mobx/mobxCounterSlice';
 import {observer} from 'mobx-react-lite';
 
 import useStyleCounter from './useStyleCounter';
